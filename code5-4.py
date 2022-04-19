dba2ea6e2ecc82cceac7cda178a4eac3ea942e32
RPKQ <rpkq0512@gmail.com>
2022-04-19 19:06:09 +0800

    code5-4 done

--- code5-4.py
+++ code5-4.py
@@ -4,7 +4,7 @@ import random, time
 bob = remote('cns.csie.org', 8016)
 
 # Bob: flag 2
-bob.sendlineafter(b"Your choice: ", str(2))
+bob.sendlineafter(b"Your choice: ", str(4))
 
 # get p, g, y, h
 bob.recvuntil(b"p = ")
@@ -16,29 +16,27 @@ y = int(bob.recvline().strip())
 bob.recvuntil(b"h = ")
 h = int(bob.recvline().strip())
 
-# get c by random(int(time.time()))
-random.randint(1, p-1)    # h
+x = 4955443778776036716256191344604504647502861925880052553262118920576335648554926844698019686656852223900095990229865138812451265325069898790688422886552274
+
+r = random.randint(1, p-2)
 
 # send z, a, b
-z = 1
-a = pow(y, -c, p)
-b = 1
+z = pow(h, x, p)
+a = pow(g, r, p)
+b = pow(h, r, p)
 bob.sendlineafter(b"z = ", str(z))
 bob.sendlineafter(b"a = ", str(a))
 bob.sendlineafter(b"b = ", str(b))
 
-# check c
-# bob.recvuntil(b"c = ")
-# c = int(bob.recvline().strip())
+# get c
+bob.recvuntil(b"c = ")
+c = int(bob.recvline().strip())
 # print("c = ", c)
 
 #
-w = p-1
+w = c*x + r 
 bob.sendlineafter(b"w = ", str(w))
 
 print(bob.recvline().strip().decode())
-
-
-
 
 
