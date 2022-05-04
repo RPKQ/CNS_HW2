15384ab81f16c6e1339865663690d0206fd4ccce
RPKQ <rpkq0512@gmail.com>
2022-05-04 19:31:17 +0800

    code7-2 finished

--- code7-2.py
+++ code7-2.py
@@ -1,7 +1,3 @@
-import socket
-import socks
-
-
 #  https://github.com/torproject/torspec/blob/main/rend-spec-v3.txt#L2029
 #
 #      onion_address = base32(PUBKEY | CHECKSUM | VERSION) + ".onion"
@@ -13,8 +9,8 @@ import socks
 #        - ".onion checksum" is a constant string
 #        - CHECKSUM is truncated to two bytes before inserting it in onion_address
 
+### GENERATE ADDRESS ###
 # import base64, hashlib
-
 # with open("hw2/tor/pubkey", "rb") as f:
 #     pk = f.read(100)[32:]
 #     H = hashlib.sha3_256()
@@ -23,9 +19,24 @@ import socks
 #     onion_address = base64.b32encode(pk+checksum+b'\x03') + b".onion"
 #     print(onion_address.decode().lower())
 
+import socks
 
-socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "localhost", 9050, True)
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050, True)
 s = socks.socksocket()
-s.connect('http://cnshwur3vd6rdqotatnt3rhhdaxiq54lury2qkpttk64dlisvmdls3qd.onion', 8002)
-# s.sendall('Hello world')
-print(s.recv(1024))
+s.connect(('cnshwur3vd6rdqotatnt3rhhdaxiq54lury2qkpttk64dlisvmdls3qd.onion', 8002))
+
+range = s.recv(1024).decode().split()[-3][-2:]
+L = 1
+R = pow(2, int(range))
+while L<R:
+    M = (L+R)//2
+    s.sendall((str(M)+'\n').encode())
+    res = s.recv(1024)
+    if b'small' in res:
+        L = M+1
+    elif b'big' in res:
+        R = M-1
+    else:
+        print(res.decode())
+        break
+        
