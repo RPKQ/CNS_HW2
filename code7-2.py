a76d2130d24008a000eba82ee4cd117325df9d09
RPKQ <rpkq0512@gmail.com>
2022-05-11 01:14:10 +0800

    code7-2 finished

--- code7-2.py
+++ code7-2.py
@@ -10,14 +10,14 @@
 #        - CHECKSUM is truncated to two bytes before inserting it in onion_address
 
 ### GENERATE ADDRESS ###
-# import base64, hashlib
-# with open("hw2/tor/pubkey", "rb") as f:
-#     pk = f.read(100)[32:]
-#     H = hashlib.sha3_256()
-#     H.update(b".onion checksum"+ pk + b'\x03') 
-#     checksum = H.digest()[:2]
-#     onion_address = base64.b32encode(pk+checksum+b'\x03') + b".onion"
-#     print(onion_address.decode().lower())
+import base64, hashlib
+with open("hw2/tor/pubkey", "rb") as f:
+    pk = f.read(100)[32:]
+    H = hashlib.sha3_256()
+    H.update(b".onion checksum"+ pk + b'\x03') 
+    checksum = H.digest()[:2]
+    onion_address = base64.b32encode(pk+checksum+b'\x03') + b".onion"
+    print("address:", onion_address.decode().lower())
 
 import socks
 
@@ -28,14 +28,19 @@ s.connect(('cnshwur3vd6rdqotatnt3rhhdaxiq54lury2qkpttk64dlisvmdls3qd.onion', 800
 range = s.recv(1024).decode().split()[-3][-2:]
 L = 1
 R = pow(2, int(range))
-while L<R:
+cnt = 0 
+while L<=R:
     M = (L+R)//2
     s.sendall((str(M)+'\n').encode())
     res = s.recv(1024)
+    cnt += 1
+    print(cnt, str(M))
     if b'small' in res:
         L = M+1
+        print(res.decode()[:-13])
     elif b'big' in res:
         R = M-1
+        print(res.decode()[:-13])
     else:
         print(res.decode())
         break
